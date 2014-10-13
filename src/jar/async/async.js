fe5a6a35e3aeea56df4af4de028b49e050f75254
HROH <h.r.o.haas@googlemail.com>
2014-10-14 00:46:35 +0200

    Fixed use of Array.from after change in behaviour

--- src/jar/async/async.js
+++ src/jar/async/async.js
@@ -7,7 +7,7 @@ JAR.register({
 
     var async = {
         wait: function(fn, wait) {
-            var args = Arr.from(arguments, 2);
+            var args = Arr.from(arguments).slice(2);
 
             window.setTimeout(function tick() {
                 fn.apply(null, args);
