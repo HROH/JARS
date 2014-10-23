7845f835d57a67df2278f6ef1d0c7a84f2f0731a
HROH <h.r.o.haas@googlemail.com>
2014-10-23 21:22:23 +0200

    Updated "jar.async.Value.M$Debuggable"
    Added possibility to use custom logger.
    Added possibility to stop debugging.
    Only have one debugging subscription at a time.

--- src/jar/async/Value/M$Debuggable.js
+++ src/jar/async/Value/M$Debuggable.js
@@ -1,41 +1,61 @@
 JAR.register({
     MID: 'jar.async.Value.M$Debuggable',
-    deps: 'jar.lang.MixIn'
-}, function(MixIn) {
+    deps: [{
+        System: ['::isA', 'Logger']
+    }, {
+        'jar.lang': ['MixIn', 'Object']
+    }]
+}, function(isA, Logger, MixIn, Obj) {
     'use strict';
 
-    var M$Debuggable = new MixIn('Debuggable', {
-        debug: function() {
-            var logger = this.Class.logger,
-                hash = this.getHash();
+    var debuggedValues = {},
+        M$Debuggable;
 
-            return this.subscribe({
-                onUpdate: function(newValue) {
-                    logger.log('Got update for ${hash} with value ${val}', {
-                        hash: hash,
-
-                        val: newValue
-                    });
+    M$Debuggable = new MixIn('Debuggable', {
+        debug: function(customLogger) {
+            var logger = isA(customLogger, Logger) ? customLogger : this.Class.logger,
+                hash = this.getHash(),
+                data = {
+                    hash: hash
                 },
+                subscription;
 
-                onError: function(newError) {
-                    logger.error('Got error for ${hash} with message ${msg}', {
-                        hash: hash,
+            if (!debuggedValues[hash]) {
+                subscription = this.subscribe({
+                    onUpdate: function(newValue) {
+                        logger.log('Got update for ${hash} with value ${val}', Obj.extend({
+                            val: newValue
+                        }, data));
+                    },
 
-                        msg: newError.message
-                    });
-                },
+                    onError: function(newError) {
+                        logger.error('Got error for ${hash} with message ${msg}', Obj.extend({
+                            msg: newError.message
+                        }, data));
+                    },
 
-                onFreeze: function() {
-                    logger.log('Froze ${hash}', {
-                        hash: hash
-                    });
-                }
-            });
+                    onFreeze: function() {
+                        logger.log('Froze ${hash}', Obj.copy(data));
+                    }
+                });
+
+                subscription && (debuggedValues[hash] = subscription);
+            }
+
+            return this;
+        },
+
+        undebug: function() {
+            var hash = this.getHash(),
+                subscription = debuggedValues[hash];
+
+            delete debuggedValues[hash];
+
+            return this.unsubscribe(subscription);
         }
     }, {
         classes: [this]
     });
-    
+
     return M$Debuggable;
 });
