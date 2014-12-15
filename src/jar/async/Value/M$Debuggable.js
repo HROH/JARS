5291047e7069f5085d2078ca552878c890e95e9f
HROH <h.r.o.haas@googlemail.com>
2014-12-15 18:35:01 +0100

    Extracted error messages as constants

--- src/jar/async/Value/M$Debuggable.js
+++ src/jar/async/Value/M$Debuggable.js
@@ -1,15 +1,17 @@
 JAR.register({
     MID: 'jar.async.Value.M$Debuggable',
     deps: [{
-        System: ['::isA', 'Logger']
-    }, {
+        System: ['::isA', 'Logger'],
         'jar.lang': ['MixIn', 'Object']
     }]
 }, function(isA, Logger, MixIn, Obj) {
     'use strict';
 
     var debuggedValues = {},
-        M$Debuggable;
+        M$Debuggable,
+        MSG_VALUE_UPDATE = 'Got update for ${hash} with value ${val}',
+        MSG_VALUE_ERROR = 'Got error for ${hash} with message ${msg}',
+        MSG_VALUE_FREEZE = 'Froze ${hash}';
 
     M$Debuggable = new MixIn('Debuggable', {
         debug: function(customLogger) {
@@ -18,28 +20,28 @@ JAR.register({
                 data = {
                     hash: hash
                 },
-                subscription;
+                subscriptionID;
 
             if (!debuggedValues[hash]) {
-                subscription = this.subscribe({
+                subscriptionID = this.subscribe({
                     onUpdate: function(newValue) {
-                        logger.log('Got update for ${hash} with value ${val}', Obj.extend({
+                        logger.log(MSG_VALUE_UPDATE, Obj.extend({
                             val: newValue
                         }, data));
                     },
 
                     onError: function(newError) {
-                        logger.error('Got error for ${hash} with message ${msg}', Obj.extend({
+                        logger.error(MSG_VALUE_ERROR, Obj.extend({
                             msg: newError.message
                         }, data));
                     },
 
                     onFreeze: function() {
-                        logger.log('Froze ${hash}', Obj.copy(data));
+                        logger.log(MSG_VALUE_FREEZE, Obj.copy(data));
                     }
                 });
 
-                subscription && (debuggedValues[hash] = subscription);
+                subscriptionID && (debuggedValues[hash] = subscriptionID);
             }
 
             return this;
