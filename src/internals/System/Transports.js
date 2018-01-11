a58bc5560fbeea76e5d11617e513a91e9c5f3b62
Holger Haas <h.r.o.haas@gmail.com>
2018-01-11 21:30:49 +0100

    Cleanup code

--- src/internals/System/Transports.js
+++ src/internals/System/Transports.js
@@ -34,9 +34,7 @@ JARS.module('System.Transports', ['Console']).meta({
          * @param {Object} transport
          */
         add: function(mode, transport) {
-            if (!hasOwnProp(transports, mode)) {
-                transports[mode] = transport;
-            }
+            hasOwnProp(transports, mode) || (transports[mode] = transport);
         },
         /**
          * @param {string} mode
@@ -45,11 +43,22 @@ JARS.module('System.Transports', ['Console']).meta({
          * @param {Object} data
          */
         write: function(mode, level, context, data) {
-            var transport = transports[mode];
-
-            transport && (transport[level] ? transport[level](context, data) : transport.write(level, context, data));
+            hasOwnProp(transports, mode) && writeToTransport(transports[mode], level, context, data);
         }
     };
+
+    /**
+     * @memberof module:Transports
+     * @inner
+     *
+     * @param {Object} transport
+     * @param {string} level
+     * @param {string} context
+     * @param {Object} data
+     */
+    function writeToTransport(transport, level, context, data) {
+        transport[level] ? transport[level](context, data) : transport.write(level, context, data);
+    }
 
     return Transports;
 });
