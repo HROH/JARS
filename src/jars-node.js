846427c54e6e6667ed1824516932e010b14c7f0f
Holger Haas <h.r.o.haas@gmail.com>
2018-04-22 11:33:56 +0200

    Add experimental entry point for nodejs

---
+++ src/jars-node.js
@@ -0,0 +1,69 @@
+(function globalSetup(envGlobal) {
+    'use strict';
+
+    var Env, SourceManager;
+
+    Env = (function envConfigSetup() {
+        /**
+         * @namespace
+         *
+         * @memberof JARS~internals
+         */
+        var Env = {
+            /**
+             * @type {Global}
+             */
+            global: envGlobal,
+            /**
+             * @type {string}
+             */
+            MAIN_MODULE: envGlobal.process.env.MAIN_MODULE,
+            /**
+             * @type {string}
+             */
+            BASE_PATH: envGlobal.process.env.BASE_PATH || './',
+            /**
+             * @type {string}
+             */
+            INTERNALS_PATH: envGlobal.process.env.INTERNALS_PATH || __dirname + '/internals/'
+        };
+
+        return Env;
+    })();
+
+    SourceManager = (function sourceManagerSetup() {
+        var SourceManager;
+
+        /**
+         * @namespace
+         *
+         * @memberof JARS~internals
+         */
+        SourceManager = {
+            /**
+             * @param {string} path
+             */
+            load: function(path) {
+                envGlobal.setTimeout(function() {
+                    try {
+                        require(require('path').join.apply(null, path.split('/')));
+                    } catch (error) {}
+                });
+            }
+        };
+
+        return SourceManager;
+    })();
+
+    envGlobal.JARS || SourceManager.load(Env.INTERNALS_PATH + 'JARS.js');
+
+    (function initJARS() {
+        envGlobal.setTimeout(function() {
+            envGlobal.JARS ? JARS.platform({
+                Env: Env,
+
+                SourceManager: SourceManager
+            }) : initJARS();
+        }, 100);
+    })();
+})(global);
